0707e3c3f8564af63c6d8a65a32b264745d8aba4
Dakshvir Rehill <dakshvirr@gmail.com>
2019-11-30 21:51:58 -0500

    Edt : Used deltaTime instead of absolute movement

--- ProjectTemplate/Player.cpp
+++ ProjectTemplate/Player.cpp
@@ -9,6 +9,7 @@ void Player::initialize()
 	spriteTexture.loadFromFile("../Assets/WitchIdle.png");
 	playerSprite.setTexture(spriteTexture);
 	playerSprite.setColor(sf::Color(255, 255, 255, 200));
+	playerSprite.setOrigin(getGameObject()->getTransform()->getPosition());
 }
 
 void Player::update(float deltaTime)
@@ -17,19 +18,19 @@ void Player::update(float deltaTime)
 
 	if (InputManager::instance().getKeyState(sf::Keyboard::Down) == InputManager::PushState::Held)
 	{
-		moveOffset.y += 5;
+		moveOffset.y += 50 * deltaTime;
 	}
 	if (InputManager::instance().getKeyState(sf::Keyboard::Up) == InputManager::PushState::Held)
 	{
-		moveOffset.y -= 5;
+		moveOffset.y -= 50 * deltaTime;
 	}
 	if (InputManager::instance().getKeyState(sf::Keyboard::Left) == InputManager::PushState::Held)
 	{
-		moveOffset.x -= 5;
+		moveOffset.x -= 50 * deltaTime;
 	}
 	if (InputManager::instance().getKeyState(sf::Keyboard::Right) == InputManager::PushState::Held)
 	{
-		moveOffset.x += 5;
+		moveOffset.x += 50 * deltaTime;
 	}
 
 	getGameObject()->getTransform()->translate(moveOffset);
